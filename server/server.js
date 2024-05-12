afcc92718ab6a2a23235814cd63b593e0d83aa92
sneha931 <153498006+sneha931@users.noreply.github.com>
2024-05-13 01:19:10 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const app=express();
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cors({
-    origin:'http://localhost:3000',
+    origin:'https://fsotp.vercel.app',
     credentials:true
 }))
 mongoose.connect(process.env.MONGO_URL)
